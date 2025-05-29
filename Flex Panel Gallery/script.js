9926c6f9497ca88318e0927f725479c1ac0e7c1f
duyguozpercin <duygucetind@gmail.com>
2025-05-29 23:58:38 +0300

    Refactor event listeners for panel interactions in script.js

--- Flex Panel Gallery/script.js
+++ Flex Panel Gallery/script.js
@@ -9,3 +9,7 @@ function toggleActive(e) {
     this.classList.toggle("open-active");
   }
 }
+panels.forEach((panel) => panel.addEventListener("click", toggleOpen));
+panels.forEach((panel) =>
+  panel.addEventListener("transitionend", toggleActive)
+);
